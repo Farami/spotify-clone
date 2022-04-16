082176380f62355bb92b1b1156c2dc8c82cbba31
Farami <webmaster@farami.de>
2022-04-16 21:40:02 +0200

    feat: only scroll playlists

--- components/organisms/Sidebar.tsx
+++ components/organisms/Sidebar.tsx
@@ -13,20 +13,20 @@ import Playlists from '../molecules/Playlists';
 
 function Sidebar() {
   return (
-    <div className="hidden h-screen overflow-y-scroll border-r border-gray-900 p-5 pb-36 text-xs text-gray-500 scrollbar-hide sm:max-w-[12rem] md:inline-flex lg:max-w-[15rem] lg:text-sm">
-      <div className="space-y-4 overflow-x-hidden scrollbar-hide">
-        <IconButton icon={HomeIcon} text="Home" />
-        <IconButton icon={SearchIcon} text="Search" />
-        <IconButton icon={LibraryIcon} text="Your Library" />
+    <div className="text-md h-screen w-64 flex-col space-y-4 border-r border-gray-900 pl-4 pt-4 pb-24 text-gray-500 md:flex">
+      <IconButton icon={HomeIcon} text="Home" />
+      <IconButton icon={SearchIcon} text="Search" />
+      <IconButton icon={LibraryIcon} text="Your Library" />
 
-        <hr className="border-t-[0.1px] border-gray-900" />
+      <hr className="border-t-[0.1px] border-gray-900" />
 
-        <IconButton icon={PlusCircleIcon} text="Create Playlist" />
-        <IconButton icon={HeartIcon} text="Liked Songs" />
-        <IconButton icon={RssIcon} text="Your Episodes" />
+      <IconButton icon={PlusCircleIcon} text="Create Playlist" />
+      <IconButton icon={HeartIcon} text="Liked Songs" />
+      <IconButton icon={RssIcon} text="Your Episodes" />
 
-        <hr className="border-t-[0.1px] border-gray-900" />
+      <hr className="border-t-[0.1px] border-gray-900" />
 
+      <div className="h-full flex-1 space-y-4 overflow-x-hidden overflow-y-scroll pb-4 pr-6 scrollbar scrollbar-thumb-gray-800 scrollbar-track-gray-900">
         <Playlists />
       </div>
     </div>
